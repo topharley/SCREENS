d7e9ebedd87c5de4518f44ddde3e2ccca2aa5419
topharley <ytopa@mail.ru>
2023-11-05 15:26:55 +0300

    Учет масштабирования при высоком разрешении

---
+++ ClipperUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Screens
+{
+    class ClipperUtils
+    {
+        private enum ProcessDPIAwareness
+        {
+            ProcessDPIUnaware = 0,
+            ProcessSystemDPIAware = 1,
+            ProcessPerMonitorDPIAware = 2
+        }
+
+        [DllImport("shcore.dll")]
+        private static extern int SetProcessDpiAwareness(ProcessDPIAwareness value);
+
+        public static void SetDpiAwareness()
+        {
+            try
+            {
+                if (Environment.OSVersion.Version.Major >= 6)
+                {
+                    SetProcessDpiAwareness(ProcessDPIAwareness.ProcessPerMonitorDPIAware);
+                }
+            }
+            catch (EntryPointNotFoundException)
+            {
+                //this exception occures if OS does not implement this API, just ignore it.
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace Screens
         [STAThread]
         static void Main()
         {
+            ClipperUtils.SetDpiAwareness();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             SetStartMode();
